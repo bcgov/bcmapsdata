0f8ac18b4eec39b4a7133b8f35c6f79eaf70f599
Andy Teucher <andy.teucher@gov.bc.ca>
2017-10-18 13:33:43 -0700

    Fix path to utils.R file

--- data-raw/process_15M_watercourses.R
+++ data-raw/process_15M_watercourses.R
@@ -10,7 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-source("data-raw/data-raw.R")
+source("data-raw/utils.R")
 
 watercourses_15M_zip <- "data-raw/canvec_15M_hydro/canvec_15M_CA_Hydro_shp.zip"
 
