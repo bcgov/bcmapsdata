7ac4e1d2a935334ddf8bd96db4990cc4921ba661
Andy Teucher <andy.teucher@gov.bc.ca>
2017-10-23 11:11:32 -0700

    Add 1:5M watercourses

---
+++ data-raw/process_5M_watercourses.R
@@ -0,0 +1,23 @@
+# Copyright 2016 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+source("data-raw/utils.R")
+
+watercourses_5M_zip <- "data-raw/canvec_5M_hydro/canvec_5M_CA_Hydro_shp.zip"
+
+unzip(watercourses_5M_zip, exdir = "data-raw/canvec_5M_hydro")
+
+watercourses_5M <- process_file("data-raw/canvec_5M_hydro/watercourse_1.shp", clip_bc = TRUE)
+
+watercourses_5M <- set_utf8(watercourses_15M)
+
+use_data(watercourses_5M, overwrite = TRUE, compress = "xz")
