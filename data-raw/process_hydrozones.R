8df179fdd27d743dac1542630bb984d28bcfe91b
Andy Teucher <andy.teucher@gmail.com>
2017-11-03 10:28:45 -0700

    Add hydrozones

---
+++ data-raw/process_hydrozones.R
@@ -0,0 +1,21 @@
+# Copyright 2017 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+source("data-raw/utils.R")
+
+hydrologic_zip <- "data-raw/hydrozones/BC_hydrologic_zones.zip"
+
+unzip(hydrologic_zip , exdir = "data-raw/hydrozones")
+
+hydrozones <- process_file("data-raw/hydrozones/HYD_BC_H_Z/HYD_BC_H_Z_polygon.shp")
+
+use_data(hydrozones, overwrite = TRUE, compress = "xz")
