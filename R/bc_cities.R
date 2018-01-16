aae4cf57ca601446f788c6b610f26088595610f1
Sam Albers <sam.albers@gov.bc.ca>
2018-01-16 14:36:33 -0800

    added bc_cities documentation

---
+++ R/bc_cities.R
@@ -0,0 +1,28 @@
+# Copyright 2018 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+#' BC Major Cities Points 1:2,000,000 (Digital Baseline Mapping)
+#'
+#' Layer for BC's major population centres. 
+#'
+#'
+#' @format An `sf` points object with the points for British Columbia's
+#'    major population centre.
+#'
+#'
+#' @source Original data from
+#'   \href{https://catalogue.data.gov.bc.ca/dataset/bc-major-cities-points-1-2-000-000-digital-baseline-mapping}{DataBC},
+#'    under the
+#'   \href{https://www2.gov.bc.ca/gov/content?id=A519A56BC2BF44E4A008B33FCF527F61}{Open
+#'   Government Licence - British Columbia}.
+#'
+"bc_cities"
