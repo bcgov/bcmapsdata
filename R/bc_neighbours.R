ca0055cbc20168eafefa19c59018230f6c878e78
Sam Albers <sam.albers@gov.bc.ca>
2018-07-12 08:54:49 -0700

    add documentation layer

---
+++ R/bc_neighbours.R
@@ -0,0 +1,27 @@
+# Copyright 2018 Province of British Columbia
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+
+#' Boundary of British Columbia and provinces/states that border British Columbia 
+#'
+#' Boundary of British Columbia and provinces/states that border British Columbia bounded by a square projected box.
+#' This layer is primarily intended for plotting purposes. 
+#'
+#'
+#' @details Downloaded from \href{https://www.naturalearthdata.com/}{Natural Earth} via
+#' \href{https://github.com/ropensci/rnaturalearth}{rnaturalearth} and clipped to the area within a bounding box
+#' then re-projected to BC Albers projection. Some attribute fields were removed, and other attribute 
+#' field names were changed.
+#'
+#'
+
+"bc_neighbours"
